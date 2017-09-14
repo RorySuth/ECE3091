75c2c8762e35719e600413d77e1620dae6b6ff86
Rory <rdsut1@student.monash.edu>
2017-09-14 12:00:23 +1000

    Stepper code has a nice function now. Call Stepper(direction, steps) to make the stepper move up or down a certain number of steps. The microswitch needs to be added for zero-ing.

--- Stepper/Design01.cydsn/main.c
+++ Stepper/Design01.cydsn/main.c
@@ -10,20 +10,64 @@
  * ========================================
 */
 #include "project.h"
+void Stepper(int direction, int steps);
+int stepperPosition = 0;
+uint8 stepperDirection = 0; //FF is down, 0 is up
+uint8 * stepperDirectionPointer = &stepperDirection;
+CY_ISR_PROTO(StepperCountHandler);
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    PWM_Start();
-    int StepperDirection = 0; //0 = up?, FFFF = down?
+    StepperPWM_Start();
+    StepperPWM_Sleep();
+    StepperCountInterrupt_StartEx(StepperCountHandler);
     
     for(;;)
     {
-        CyDelay(1000);
-        StepperDirection = ~StepperDirection;
-        StepperDirection_Write(StepperDirection);
+        Stepper(0, 200);
+        CyDelay(500);
+        Stepper(0, 200);
+        CyDelay(500);
+        Stepper(0, 200);
+        CyDelay(500);
+        Stepper(1, 200);
+        CyDelay(500);
+        Stepper(1, 200);
+        CyDelay(500);
+        Stepper(1, 200);
+        CyDelay(500);
     }
 }
 
+void Stepper(int direction, int steps) {
+    int lastStepperPosition = stepperPosition; //Grabs the current global position before the stepper moves
+    *stepperDirectionPointer = direction; //Pass the direction out of the function so the interrupt works
+    StepperDirection_Write(direction);
+    
+    if (direction == 0) { //if the steppe is going up leave steps as positive
+    }
+    else {               //if the stepper is going down, change steps to -ve for easier calculation
+        steps = -steps;
+    }
+    
+    StepperPWM_Wakeup();
+    while (stepperPosition != (lastStepperPosition + steps)) { //Checks to see if the global stepper position is equal to the desired end position.
+    }
+    
+    StepperPWM_Sleep(); //Turn off the stepper
+    
+    
+}
+
+CY_ISR(StepperCountHandler){
+    StepperPWM_ReadStatusRegister(); //Clear interrupt
+    if (stepperDirection == 0) {
+        ++stepperPosition; //Increment Stepper
+    }
+    else {
+        --stepperPosition;
+    }
+}
 /* [] END OF FILE */
